2bc8a9bbccb381fb30d4bd315f8d01e52ac989e9
zhangheng <zhangheng@allinmd.cn>
2017-10-26 11:08:26 +0800

    vue学习笔记

---
+++ vue-webpack-demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div>{{msg}}</div>
+    <News></News>
+  </div>
+</template>
+
+<script>
+  import News from './components/News.vue'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        'msg': 'webpack-vue-demo'
+      }
+    },
+    components: {
+      News: News
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
